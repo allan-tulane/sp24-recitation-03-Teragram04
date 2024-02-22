23a5e94808b8916fea9da51345565d9e330fd417
Teragram04 <91078893+Teragram04@users.noreply.github.com>
2024-02-22 00:42:27 +0000

    changed test_quadratic_multiply to runtime_quadratic_multiply

--- main.py
+++ main.py
@@ -77,11 +77,10 @@ def _quadratic_multiply(x, y):
     return BinaryNumber(mh.decimal_val + m1m.decimal_val + m2m.decimal_val +
                         l.decimal_val)
 
-pass
 
-def test_quadratic_multiply(x, y, f):
+def runtime_quadratic_multiply(x, y, f):
   start = time.time()
   # multiply two numbers x, y using function f
   _quadratic_multiply(x, y)
-  
+
   return (time.time() - start) * 1000
